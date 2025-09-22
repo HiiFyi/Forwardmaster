f7b18b358592c518484a78fa6d3ce5454a4aa91c
owajbhai <owajshaikhrcm@gmail.com>
2025-09-22 10:48:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@ AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('
 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "23621595"))
+    API_HASH = environ.get("API_HASH", "de904be2b4cd4efe2ea728ded17ca77d")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "PrimeXBots") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Prime-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://mageerauldvjo8d:O3cGzJqMo1e5YVXA@cluster0.wdeqnro.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "1249672673"))
 
 
 
@@ -25,5 +25,6 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
+
 
 
